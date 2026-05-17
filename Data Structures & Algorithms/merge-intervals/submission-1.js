e9e49acbef78c72d5cad9dc57ea8f29e46e260fb
VRR <45332960+venkat090993@users.noreply.github.com>
2026-05-17 16:57:32 +0530

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        const sorted_intervals = intervals.sort((a,b)=> a[0] - b[0]);
+        const stack = [sorted_intervals[0]];
+        for(let i = 1; i<sorted_intervals.length; i++){
+            const prev_start = stack[stack.length - 1][0];
+            const prev_end = stack[stack.length - 1][1];
+
+            const curr_start = sorted_intervals[i][0];
+            const curr_end = sorted_intervals[i][1];
+
+            if(curr_start > prev_end){
+                stack.push(sorted_intervals[i]);
+            }else{
+                stack.pop();
+                const new_interval_start = Math.min(prev_start, curr_start);
+                const new_interval_end = Math.max(prev_end, curr_end);
+                stack.push([new_interval_start,new_interval_end]);
+            }
+        }
+        return stack;
+    }
+}
