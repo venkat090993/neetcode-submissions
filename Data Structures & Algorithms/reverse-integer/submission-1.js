9bff6be3696d727c388034d55d8baad9ca90e74e
VRR <45332960+venkat090993@users.noreply.github.com>
2026-05-17 15:21:18 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    reverse(x) {
+        let reverse = ""
+        let isNegative = false;
+        if(x > 0){
+            reverse = `${x}`.split('').reverse().join('');
+        }
+
+        if(x < 0){
+        isNegative = true
+        reverse = `${x}`.slice(1).split('').reverse().join('');
+        }
+
+        const parsed = Number(reverse);
+        if(parsed > 2147483648 || parsed < -2147483648){
+            return 0
+        }
+
+        return isNegative ? Number(`-${parsed}`) : parsed;
+    }
+}
